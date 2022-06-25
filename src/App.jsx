aa7e49c98b3fed834bbf76039237e98156c63f2a
Abhisek0x <imabhisekyadav@gmail.com>
2022-06-25 20:46:23 +0530

    added web3 wallet connect

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
-import './assets/style/App.css'
-import World from './components/World'
+import World from "./components/World";
+import { useMoralis } from "react-moralis";
+import { ConnectButton } from "web3uikit";
+
+const supportedChains = ["4"];
 
 function App() {
-  return (
-    <div className="App">
-      <World/>
-    </div>
-  )
+	const { isWeb3Enabled, chainId } = useMoralis();
+	return (
+		<div>
+			<div className="float-right py-2 px-4 pr-10">
+				<ConnectButton moralisAuth={false} />
+			</div>
+
+			{isWeb3Enabled ? (
+				<div>
+					{supportedChains.includes(parseInt(chainId).toString()) ? (
+						<div className="App">
+							<World />
+						</div>
+					) : (
+						<div>{`Please switch to a supported chainId. The supported Chain Ids are: ${supportedChains}`}</div>
+					)}
+				</div>
+			) : (
+				<div>Please connect to a Wallet</div>
+			)}
+		</div>
+	);
 }
 
-export default App
+export default App;
